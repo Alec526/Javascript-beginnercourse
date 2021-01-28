b85ad795b25385f0c3b84c694ab0b5b9ed71c639
Alec526 <aarnaoutoglou@roc-dev.com>
2021-01-28 12:10:21 +0100

    H2.1 af

---
+++ h02/Opdracht2.1/javascript.js
@@ -0,0 +1,5 @@
+
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subkop").innerText = "Mijn geweldige subkop";
+document.getElementById("image1").src = "img/image1.jpg";
+document.getElementById("image2").src = "img/image1.jpg";
