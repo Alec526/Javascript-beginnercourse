550dd1b9e337c9a76aec2b283333e357e403eeda
Alec526 <aarnaoutoglou@roc-dev.com>
2021-01-28 13:34:28 +0100

    H2.2 af

---
+++ h02/Opdracht2.2/javascript.js
@@ -0,0 +1,14 @@
+document.getElementById("blok1").style.backgroundColor = "turquoise";
+document.getElementById("blok2").style.backgroundColor = "green";
+document.getElementById("blok3").style.backgroundColor = "yellow";
+document.getElementById("blok4").style.backgroundColor = "red";
+
+document.getElementById("blok2").style.position = "relative";
+document.getElementById("blok2").style.top = "20px";
+document.getElementById("blok3").style.position = "relative";
+document.getElementById("blok3").style.top = "60px";
+
+document.getElementById("blok1").style.paddingTop = "20px";
+document.getElementById("blok1").style.textAlign = "center";
+document.getElementById("blok2").style.textAlign = "center";
+document.getElementById("blok3").style.textAlign = "right";
