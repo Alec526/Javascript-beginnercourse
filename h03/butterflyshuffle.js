3a42e4b4adc34e682bb95f81715ad2592ed2f4ae
Alec526 <aarnaoutoglou@roc-dev.com>
2021-01-30 16:22:21 +0100

    H3 af

---
+++ h03/butterflyshuffle.js
@@ -0,0 +1,18 @@
+var fotoArray = document.getElementsByTagName("img");
+var random = 0;
+var randomNummer = [];
+
+
+while(randomNummer.length < 9) {
+    random = Math.floor(Math.random() * 9) + 1;
+    if(randomNummer.lastIndexOf(random) == -1) {
+        randomNummer.push(random);
+    }
+}
+
+
+random = 0;
+for(var nummer in fotoArray) {
+    fotoArray[nummer].src = "./img/img" + randomNummer[random] + ".jpg";
+    random++;
+}
