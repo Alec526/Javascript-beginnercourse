67eac3c99dda06f77bcf6501e41f03c65153f249
Alec526 <aarnaoutoglou@roc-dev.com>
2021-03-04 14:05:28 +0100

    H6 af

--- h06/memorygame.js
+++ h06/memorygame.js
@@ -3,6 +3,7 @@ const fotoArray = document.getElementsByTagName("img");
 var random = 0;
 var randomNumbers = [];
 let currentlyClickedCards = [];
+let score = 0;
 
 while(randomNumbers.length < 18) {
     random = Math.floor(Math.random() * 18) + 1;
@@ -10,48 +11,53 @@ while(randomNumbers.length < 18) {
         randomNumbers.push(random);
     }
 }
-
+/*
 random = 0;
 for(var nummer in fotoArray) {
+    console.log(randomNumbers[random]);
     fotoArray[nummer].src = "./img/img" + randomNumbers[random] + ".jpg";
     random++;
 }
-
+*/
 initGame();
 function initGame(){
     const randomNumbersArray = shuffle(generateRandomNumbers(9, 2));
     for (let i = 0; i < fotoArray.length; i++){
-        fotoArray[i].src = '.img/img${randomNumbersArray[i]}.jpg';
-        fotoArray[i].src = '.img/blank.jpg';
+        fotoArray[i].src = `./img/img${randomNumbersArray[i]}.jpg`;
+        fotoArray[i].src = `./img/blank.jpg`;
         fotoArray[i].twinNumber = randomNumbersArray[i];
+        fotoArray[i].id = i;
         fotoArray[i].addEventListener("click", cardClicked);
     }
 }
 
 function cardClicked(){
+    document.getElementById("score").innerText = "Score: "+score+"";
+    if (currentlyClickedCards.length === 1 && currentlyClickedCards[0].id === this.id) return;
     if (currentlyClickedCards.length === 1) {
         currentlyClickedCards[1] = this;
-        this.src = '.img/img${this.twinNumber}.jpg';
+        this.src = `./img/img${this.twinNumber}.jpg`;
         if (currentlyClickedCards[0].twinNumber === currentlyClickedCards[1].twinNumber){
             currentlyClickedCards = [];
+            score++;
+            console.log(score);
         } else {
             setTimeout(function (){
-                currentlyClickedCards[0].src = '.img/blank.jpg';
-                currentlyClickedCards[1].src = '.img/blank.jpg';
+                currentlyClickedCards[0].src = `./img/blank.jpg`;
+                currentlyClickedCards[1].src = `./img/blank.jpg`;
                 currentlyClickedCards = [];
             }, 1000);
         }
     } else if (currentlyClickedCards.length === 0) {
         currentlyClickedCards[0] = this;
-        this.src = '.img/img${this.twinNumber}.jpg';
+        this.src = `./img/img${this.twinNumber}.jpg`;
     }
 }
 
 function generateRandomNumbers (numberOfDifferentImages, times) {
     const returnArray = [];
     for (let j = 0; j < times; j++) {
-        for(let i = 1; i < numberOfDifferentImages; i++){
-
+        for(let i = 1; i <= numberOfDifferentImages; i++){
             returnArray.push(i);
 
         }
