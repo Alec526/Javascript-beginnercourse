d204af7060ab476bc472a0bea0ba4dba52cacd19
Alec526 <aarnaoutoglou@roc-dev.com>
2021-04-08 11:11:20 +0200

    new exercises

---
+++ test/test5/colorscheme.js
@@ -0,0 +1,17 @@
+let body = document.getElementById("background");
+
+document.getElementById("red").addEventListener("click", function(){
+    body.style.backgroundColor = "red";
+});
+
+document.getElementById("green").addEventListener("click", function(){
+    body.style.backgroundColor = "green";
+});
+
+document.getElementById("blue").addEventListener("click", function(){
+    body.style.backgroundColor = "blue";
+});
+
+document.getElementById("purple").addEventListener("click", function(){
+    body.style.backgroundColor = "purple";
+});
